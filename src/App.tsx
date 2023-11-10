def07b84429e2f46d637f10eebce76807a7c6baf
oxana737 <oxanashepel737@gmail.com>
2023-11-10 16:20:17 +0200

    add routing, add base structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import './globals.css';
+import {Route, Routes} from "react-router-dom";
+import SignInForm from "./_auth/forms/SignInForm.tsx";
+import SignUpForm from "./_auth/forms/SignUpForm.tsx";
+import {Home} from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout.tsx";
+import RootLayout from "./_root/RootLayout.tsx";
+
 const App = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <main className='flex h-screen'>
+            <Routes>
+                {/*public routes*/}
+                <Route element={<AuthLayout/>}>
+                    <Route path='/sign-in' element={<SignInForm/>}/>
+                    <Route path='/sign-up' element={<SignUpForm/>}/>
+                </Route>
+                {/*private routes*/}
+                <Route element={<RootLayout/>}>
+                    <Route index element={<Home/>}/>
+                </Route>
+            </Routes>
+        </main>
     )
 }
 export default App
