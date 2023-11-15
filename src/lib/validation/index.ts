a6f5eab0e846668813dd2a1b01210e41737a2997
oxana737 <oxanashepel737@gmail.com>
2023-11-15 14:34:54 +0200

    create sign in component

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -6,3 +6,8 @@ export const signUpValidation = z.object({
     email: z.string().email(),
     password: z.string().min(8, 'Password must be at least 8 characters.')
 })
+
+export const signInValidation = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, 'Password must be at least 8 characters.')
+})
