528ade512e556ab100a2f5bdf1fd60961ef6bf7b
oxana737 <oxanashepel737@gmail.com>
2023-11-16 12:13:26 +0200

    fix link url names

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -144,7 +144,7 @@ const SignUpForm = () => {
                     </Button>
                     <p className='text-small-regular text-light-2 text-center mt-2'>
                         Already have an account?
-                        <Link to='/sing-in' className='text-primary-500 text-small-semibold ml-1'>
+                        <Link to='/sign-in' className='text-primary-500 text-small-semibold ml-1'>
                             Log in
                         </Link>
                     </p>
