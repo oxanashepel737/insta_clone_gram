3e6e209c1ff7833f82c5f782f92dcd5e3352ab8d
oxana737 <oxanashepel737@gmail.com>
2023-11-16 15:18:47 +0200

    create bottom bar component, add structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import './globals.css';
 import {Route, Routes} from "react-router-dom";
 import SignInForm from "./_auth/forms/SignInForm.tsx";
 import SignUpForm from "./_auth/forms/SignUpForm.tsx";
-import {Home} from "./_root/pages";
+import {Home, AllUsers, CreatePost, EditPost, Explore, PostDetails, Profile, Saved, UpdateProfile, LikedPosts} from "./_root/pages";
 import AuthLayout from "./_auth/AuthLayout.tsx";
 import RootLayout from "./_root/RootLayout.tsx";
-import { Toaster } from "@/components/ui/toaster"
+import {Toaster} from "@/components/ui/toaster"
 
 const App = () => {
     return (
@@ -19,6 +19,15 @@ const App = () => {
                 {/*private routes*/}
                 <Route element={<RootLayout/>}>
                     <Route index element={<Home/>}/>
+                    <Route path='/explore' element={<Explore/>}/>
+                    <Route path='/saved' element={<Saved/>}/>
+                    <Route path='/all-users' element={<AllUsers/>}/>
+                    <Route path='/create-post' element={<CreatePost/>}/>
+                    <Route path='/update-post/:id' element={<EditPost/>}/>
+                    <Route path='/posts/:id' element={<PostDetails/>}/>
+                    <Route path='/profile/:id/*' element={<Profile/>}/>
+                    <Route path='/update-profile/:id' element={<UpdateProfile/>}/>
+                    <Route path='/liked-posts/:id' element={<LikedPosts/>}/>
                 </Route>
             </Routes>
             <Toaster/>
--- src/components/shared/BottomBar.tsx
+++ src/components/shared/BottomBar.tsx
@@ -1,7 +1,39 @@
+import {Link, useLocation} from "react-router-dom";
+import {bottomBarLinks} from "@/constants";
+import {INavLink} from "@/types";
 
 const BottomBar = () => {
+    const {pathname} = useLocation();
     return (
-        <div>BottomBar</div>
+        <section className='bottom-bar'>
+            {
+                bottomBarLinks.map((link: INavLink) => {
+                    const isActive = pathname === link.route;
+                    return (
+                        <Link
+                            to={link.route}
+                            key={link.label}
+                            className={`${
+                                isActive && 'bg-primary-500 rounded-[10px]'
+                            } flex-center flex-col gap-1 p-2 transition`}
+                        >
+                            <img
+                                src={link.imgURL}
+                                alt={link.label}
+                                width={16}
+                                height={16}
+                                className={`group-hover:invert-white ${
+                                    isActive && 'invert-white'
+                                }`}
+                            />
+                            <p className='tiny-medium text-light-2'>
+                                {link.label}
+                            </p>
+                        </Link>
+                    )
+                })
+            }
+        </section>
     )
 }
 export default BottomBar
